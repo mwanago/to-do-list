feb6dd93dd381f17d5b481f8263b18ab550c9fb2
Marcin <wanago.marcin@gmail.com>
2024-05-04 23:44:06 +0200

    feat: remove dropped elements

--- src/ToDoList/useToDoList.tsx
+++ src/ToDoList/useToDoList.tsx
@@ -10,11 +10,37 @@ export function useToDoList() {
   >(null);
 
   const addToDoItem = (text: string) => {
+    if (toDoItems.includes(text)) {
+      return;
+    }
     setToDoItems([...toDoItems, text]);
   };
 
+  const removeToDoItem = (text: string) => {
+    const index = toDoItems.indexOf(text);
+    if (index === -1) {
+      return;
+    }
+    const newItems = [...toDoItems];
+    newItems.splice(index, 1);
+    setToDoItems(newItems);
+  };
+
   const addCompletedItem = (text: string) => {
+    if (completedItems.includes(text)) {
+      return;
+    }
     setCompletedItems([...completedItems, text]);
+  };
+
+  const removeCompletedItem = (text: string) => {
+    const index = completedItems.indexOf(text);
+    if (index === -1) {
+      return;
+    }
+    const newItems = [...completedItems];
+    newItems.splice(index, 1);
+    setCompletedItems(newItems);
   };
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -33,12 +59,14 @@ export function useToDoList() {
   const handleDropOnCompleted = () => {
     if (currentlyDraggedItem) {
       addCompletedItem(currentlyDraggedItem);
+      removeToDoItem(currentlyDraggedItem);
     }
   };
 
   const handleDropOnToDo = () => {
     if (currentlyDraggedItem) {
       addToDoItem(currentlyDraggedItem);
+      removeCompletedItem(currentlyDraggedItem);
     }
   };
 
